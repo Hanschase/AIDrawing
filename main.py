e8bf58104d3ccbedb8f209599c5d59adaceff488
Hanschase <101171938+Hanschase@users.noreply.github.com>
2024-11-27 16:23:58 +0800

    Update main.py
    更新函数主体内容

--- main.py
+++ main.py
@@ -1,49 +1,52 @@
 from pkg.plugin.context import register, handler, llm_func, BasePlugin, APIHost, EventContext
 from pkg.plugin.events import *  # 导入事件类
+from pkg.platform.types import *
+import re
+from plugins.HelloPlugin.get_image import download_image
 
 
 # 注册插件
-@register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
-class MyPlugin(BasePlugin):
-
-    # 插件加载时触发
+@register(name="AIDrawing", description="使用function calling函数实现AI画图的功能，并自带图像发送", version="0.1", author="Hanschase")
+class Fct(BasePlugin):
     def __init__(self, host: APIHost):
         pass
 
-    # 异步初始化
-    async def initialize(self):
-        pass
+    @llm_func(name="test")
+    async def _(self,query, keywords: str)->str:
+        """Call this function to draw something before you answer any questions.
+        - Expand the user's description into a more elaborate and detailed English prompt suitable for AI image generation, including adding details like camera aperture and specific scene descriptions, and then input the enhanced description.
+        - You will be will become reticent.
+        - Use the English keywords.
+        - Try to use short keywords as much as possible
 
-    # 当收到个人消息时触发
-    @handler(PersonNormalMessageReceived)
-    async def person_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 PersonNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
+        Args:
+            keywords: The enhanced description.
 
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
+        Returns:
+            img: The generated image.
+        """
+        self.ap.logger.info(f"优化后关键词,{keywords}")
+        img = "https://image.pollinations.ai/prompt/" + keywords
+        return img
 
-            # 回复消息 "hello, <发送者id>!"
-            ctx.add_return("reply", ["hello, {}!".format(ctx.event.sender_id)])
+    #发送图片
+    @handler(NormalMessageResponded)
+    async def convert_message(self, ctx: EventContext):
+        message = ctx.event.response_text
+        image_pattern = re.compile(r'(https://image[^\s)]+)')
+        #如果匹配到了image_pattern
+        if image_pattern.search(message):
+            url = image_pattern.search(message).group(1)
+            try:
+                #去除url末尾的句号或者括号
+                if url.endswith('.') or url.endswith(')'):
+                    url = url[:-1]
+                self.ap.logger.info(f"正在发送图片图片...{url}")
+                ctx.add_return('reply', MessageChain([Image(url=url)]))
+            except Exception as e:
+                await ctx.send_message(ctx.event.launcher_type, str(ctx.event.launcher_id),MessageChain([f"发生了一个错误：{e}"]))
+        else:
+            return ctx.add_return('reply', message)
 
-            # 阻止该事件默认行为（向接口获取回复）
-            ctx.prevent_default()
-
-    # 当收到群消息时触发
-    @handler(GroupNormalMessageReceived)
-    async def group_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 GroupNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
-
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
-
-            # 回复消息 "hello, everyone!"
-            ctx.add_return("reply", ["hello, everyone!"])
-
-            # 阻止该事件默认行为（向接口获取回复）
-            ctx.prevent_default()
-
-    # 插件卸载时触发
     def __del__(self):
         pass
